62ce55a99cc142938a435b3e46cefc58bcbbea72
ATAKAN SOLAK <atakansolak28@hotmail.com>
2023-05-12 21:31:54 +0300

    better training

--- training.py
+++ training.py
@@ -1,26 +1,43 @@
-from tqdm import tqdm
-from sklearn.model_selection import train_test_split
+#!/usr/bin/env python3
 from tensorflow.keras import layers
-from dotenv import load_dotenv
-import tensorflow as tf
+import os
 import numpy as np
 import chess.pgn
-import os
 import time
+import tensorflow as tf
+import chess
 
-load_dotenv()
-games_dir = os.getenv("GAMES_DIR")
 
-pgn_file_path = f"{games_dir}"
+def get_dataset(num_samples=None, total_samples_limit=None):
+    X, Y = [], []
+    gn = 0
+    total_samples = 0 
+    values = {'1/2-1/2': 0, '0-1': -1, '1-0': 1}
+    with open(r'C:/Users/ataka/Desktop/games.pgn') as pgn:
+        while True:
+            game = chess.pgn.read_game(pgn)
+            if game is None:
+                break
+            res = game.headers['Result']
+            if res not in values:
+                continue
+            value = values[res]
+            board = game.board()
+            for i, move in enumerate(game.mainline_moves()):
+                if total_samples_limit is not None and total_samples >= total_samples_limit: 
+                    return
+                board.push(move)
+                ser = board_to_feature_vector(board)
+                X.append(ser)
+                Y.append(value)
+                total_samples += 1  
+                if num_samples is not None and len(X) >= num_samples:
+                    yield np.array(X), np.array(Y)
+                    X, Y = [], [] 
+            print("parsing game %d, got %d examples" % (gn, len(X)))
+            gn += 1
 
-with open(pgn_file_path, "r") as pgn_file:
-    games = []
 
-    while True:
-        game = chess.pgn.read_game(pgn_file)
-        if game is None:
-            break
-        games.append(game)
 
 def board_to_feature_vector(board):
     piece_map = board.piece_map()
@@ -33,6 +50,7 @@ def board_to_feature_vector(board):
         feature_vector[row, col, layer] = 1
 
     return feature_vector
+
 
 def create_cnn(input_shape=(8, 8, 12)):
     model = tf.keras.Sequential([
@@ -50,99 +68,26 @@ def create_cnn(input_shape=(8, 8, 12)):
     model.compile(optimizer="adam", loss="mean_squared_error", metrics=["mean_absolute_error"])
     return model
 
-cnn_model = create_cnn()
-
-def data_stream(pgn_file_path, batch_size, max_moves_per_game=50):
-    with open(pgn_file_path, "r") as pgn_file:
-        while True:
-            game = chess.pgn.read_game(pgn_file)
-            if game is None:
-                break
-
-            X = []
-            y = []
-
-            board_state = game.board().fen()  # Store the board state as a string
-            result = {"1-0": 1, "0-1": -1, "1/2-1/2": 0}[game.headers["Result"]]
-
-            for i, move in enumerate(game.mainline_moves()):
-                if i >= max_moves_per_game:
-                    break
-
-                board = chess.Board(board_state)
-                board.push(move)
-                board_state = board.fen()
-
-                X.append(board_to_feature_vector(board))
-                y.append(result)
-
-                if len(X) == batch_size:
-                    yield np.array(X), np.array(y)
-                    X.clear()
-                    y.clear()
+def train_model(model, X, Y):
+    model.fit(X, Y, epochs=10, batch_size=64, validation_split=0.2)
 
 
+if __name__ == "__main__":
+    cnn_model = create_cnn()
+    start_time = time.time()
+    batch_rounds = 0  
+    total_examples = 0  
 
-
-batch_size = 16
-num_games = 1
-
-# Measure the start time
-start_time = time.time()
-
-for X_batch, y_batch in data_stream(pgn_file_path, batch_size, max_moves_per_game=30):
-    cnn_model.train_on_batch(X_batch, y_batch)
-
-
-
-# Measure the end time
-end_time = time.time()
-
-# Calculate the elapsed time
-training_time = end_time - start_time
-print("Training time:", training_time, "seconds")
-
-cnn_model.save("chess_cnn_model.h5")
-loaded_model = tf.keras.models.load_model("chess_cnn_model.h5")
-
-def evaluate_position(model, board):
-    feature_vector = board_to_feature_vector(board)
-    input_data = np.expand_dims(feature_vector, axis=0)
-    return model.predict(input_data)[0][0]
-
-def alpha_beta_search(model, board, depth, alpha, beta, maximizing_player):
-    if depth == 0 or board.is_game_over():
-        return evaluate_position(model, board), None
-
-    best_move = None
-
-    if maximizing_player:
-        max_eval = -float("inf")
-        for move in board.legal_moves:
-            board.push(move)
-            eval_score, _ = alpha_beta_search(model, board, depth - 1, alpha, beta, False)
-            board.pop()
-            if eval_score > max_eval:
-                max_eval = eval_score
-                best_move = move
-            alpha = max(alpha, eval_score)
-            if beta <= alpha:
-                break
-        return max_eval, best_move
-    else:
-        min_eval = float("inf")
-        for move in board.legal_moves:
-            board.push(move)
-            eval_score, _ = alpha_beta_search(model, board, depth - 1, alpha, beta, True)
-            board.pop()
-            if eval_score < min_eval:
-                min_eval = eval_score
-                best_move = move
-            beta = min(beta, eval_score)
-            if beta <= alpha:
-                break
-        return min_eval, best_move
-
-def find_best_move(model, board, depth=3):
-    _, best_move = alpha_beta_search(model, board, depth, -float("inf"), float("inf"), board.turn)
-    return best_move
+    try:
+        for X, Y in get_dataset(10000, 2000000):  # generate batches of 10000, stop after 2000000 total samples
+            cnn_model.fit(X, Y, epochs=10)
+            batch_rounds += 1 
+            total_examples += len(X) 
+            elapsed_time = time.time() - start_time  
+            print(f"Finished batch round {batch_rounds}. Total examples: {total_examples}. Elapsed time: {elapsed_time:.2f} seconds.")
+    except KeyboardInterrupt:
+        print("Interrupted, saving model...")
+    finally:
+        elapsed_time = time.time() - start_time 
+        print(f"Total examples processed: {total_examples}. Total elapsed time: {elapsed_time:.2f} seconds.")
+        cnn_model.save('trained_model.h5')
